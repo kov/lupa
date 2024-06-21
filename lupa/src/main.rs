ae806b1bbe1597eae5502aa81f35888176432b27
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2024-06-20 21:53:24 -0300

    Keep track of all events and print at exit

--- lupa/src/main.rs
+++ lupa/src/main.rs
@@ -1,6 +1,7 @@
 #![feature(slice_split_once)]
 use std::collections::HashMap;
 use std::env;
+use std::path::PathBuf;
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
@@ -82,19 +83,26 @@ async fn main() -> Result<(), anyhow::Error> {
     let (tx, rx): (Sender<Event>, Receiver<Event>) = mpsc::channel();
     let _ = std::thread::spawn(|| trace::run(bpf, tx));
 
-    let currently_open = Arc::new(Mutex::new(HashMap::<(u64, i64), Event>::new()));
+    let all_events = Arc::new(Mutex::new(vec![]));
+    let currently_open = Arc::new(Mutex::new(HashMap::<(u64, i64), PathBuf>::new()));
+
+    let t_all_events = all_events.clone();
     let t_currently_open = currently_open.clone();
     let _ = std::thread::spawn(move || loop {
         let event = rx.recv().unwrap();
         println!("Event received: {:?}", event);
+
         let mut currently_open = t_currently_open.lock().unwrap();
         match event.detail {
-            EventDetail::FileOpen { fd, .. } => {
-                if currently_open.insert((event.pid, fd), event).is_some() {
+            EventDetail::FileOpen { fd, ref path } => {
+                if currently_open
+                    .insert((event.pid, fd), path.clone())
+                    .is_some()
+                {
                     debug!("tried to insert duplicate fd {}", fd);
                 }
             }
-            EventDetail::FailedFileOpen { errno, path } => {
+            EventDetail::FailedFileOpen { errno, ref path } => {
                 debug!(
                     "attempt to open file path {:?} failed with error {}",
                     path, errno
@@ -109,12 +117,17 @@ async fn main() -> Result<(), anyhow::Error> {
                 }
             }
         }
+        drop(currently_open);
+
+        t_all_events.lock().unwrap().push(event);
     });
 
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
     println!("\nCurrently open:");
     println!("{:?}", currently_open.lock().unwrap());
+    println!("\nAll events:");
+    println!("{:?}", all_events.lock().unwrap());
     info!("Exiting...");
 
     Ok(())
